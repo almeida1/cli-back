2df93d02021d8fdcc029f28e394b3f4cb1632e03
almeida1 <engsoftware1@gmail.com>
2024-10-20 14:42:12 -0300

    funcao - cadastrar produto

--- sigvs-back/src/main/java/com/fatec/sigvs_back/controller/APIProdutoController.java
+++ sigvs-back/src/main/java/com/fatec/sigvs_back/controller/APIProdutoController.java
@@ -1,5 +1,50 @@
 package com.fatec.sigvs_back.controller;
 
+import java.util.Optional;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fatec.sigvs_back.model.Produto;
+import com.fatec.sigvs_back.servico.IProdutoServico;
+@CrossOrigin("*") // desabilita o cors do spring security
+@RestController
+@RequestMapping("/api/v1/produtos")
 public class APIProdutoController {
+	Logger logger = LogManager.getLogger(this.getClass());
+	@Autowired
+	private IProdutoServico produtoServico;
+
+	record ProdutoDTO(String descricao, String categoria, String custo, String quantidadeNoEstoque) {
+	};
+
+	@PostMapping
+	public ResponseEntity<Object> cadastrar(@RequestBody ProdutoDTO p) {
+		logger.info(">>>>>> apicontroller cadastrar produto iniciado...");
+		try {
+			Produto produtoNovo = new Produto();
+			produtoNovo.setDescricao(p.descricao);
+			produtoNovo.setCategoria(p.categoria);
+			produtoNovo.setCusto(p.custo);
+			produtoNovo.setQuantidadeNoEstoque(p.quantidadeNoEstoque);
+			Optional<Produto> produto = produtoServico.cadastrar(produtoNovo);
+			if (produto.isPresent()) {
+				return ResponseEntity.status(HttpStatus.CREATED).body(produto.get());
+			} else {
+				logger.info(">>>>>> api produto controller cadastrar exception =>");
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro não esperado ");
+			}
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+		}
+	}
 
 }
--- sigvs-back/src/main/java/com/fatec/sigvs_back/servico/IProdutoRepository.java
+++ sigvs-back/src/main/java/com/fatec/sigvs_back/servico/IProdutoRepository.java
@@ -1,10 +1,14 @@
 package com.fatec.sigvs_back.servico;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.fatec.sigvs_back.model.Produto;
 @Repository
 public interface IProdutoRepository extends JpaRepository<Produto, Long> {
-	
+	 // Método personalizado para calcular o valor total financeiro imobilizado no
+    // estoque
+    @Query("SELECT SUM(p.custo * p.quantidadeNoEstoque) FROM Produto p")
+    Double calcularTotalCustoQuantidade();
 }
